e8429d87b0bb7f5e20720ce22b52a67db85bee90
professorik <36134599+professorik@users.noreply.github.com>
2024-05-12 18:23:39 +0300

    icons

--- src/mock/social-networks.ts
+++ src/mock/social-networks.ts
@@ -1,20 +1,32 @@
 import YoutubeSvg from '../assets/img/youtube.svg'
-import GithubSvg from '../assets/img/github.svg'
+import XSvg from '../assets/img/x.svg'
+import TelegramSvg from '../assets/img/telegram.svg'
+import DiscordSvg from '../assets/img/discord.svg'
 import InstagramSvg from '../assets/img/instagram.svg'
 export const socialNetworks = [
     {
-        icon: GithubSvg,
-        name: 'instagram',
-        href: 'https://github.com/Ducklings-Studio',
+        icon: XSvg,
+        name: 'x',
+        href: 'https://www.twitter.com/DucklingsS87723',
     },
     {
         icon: InstagramSvg,
-        name: 'github',
+        name: 'instagram',
         href: 'https://www.instagram.com/ducklingsstudio/',
     },
     {
         icon: YoutubeSvg,
         name: 'youtube',
         href: 'https://www.youtube.com/@DucklingsStudio',
+    },
+    {
+        icon: DiscordSvg,
+        name: 'discord',
+        href: 'https://www.discord.com/invite/qxTQCBFaVr',
+    },
+    {
+        icon: TelegramSvg,
+        name: 'telegram',
+        href: 'https://t.me/DucklingsStudio',
     }
 ]
